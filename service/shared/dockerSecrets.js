853242e0639fa6f87593c4fa37497b728a74ba47
Colin C Cook <colinccook@gmail.com>
2017-06-26 20:15:30 +0100

    allow for keys to be delivered as environment variables

--- service/shared/dockerSecrets.js
+++ service/shared/dockerSecrets.js
@@ -2,6 +2,11 @@ var fs = require('fs');
 
 (function() {
     function getSecret(secretKey) {
+        // first try Docker container (docker cloud)
+        if (process.env[secretKey])
+            return process.env[secretKey].trim();
+
+        // then try secret file (docker compose v3)
         var fileContent = fs.readFileSync('/run/secrets/' + secretKey, 'utf8');
         
         if (fileContent)
